22e56d0668e4966a7c3cfe9456bea4d4083a6c13
炎冬 <enoshwang@foxmail.com>
2025-05-14 12:27:17 +0800

    基于 PullToRefreshBox 实现下拉刷新

--- app/app/src/main/java/com/hwzy/app/ui/screens/home/NewsContent.kt
+++ app/app/src/main/java/com/hwzy/app/ui/screens/home/NewsContent.kt
@@ -1,18 +1,49 @@
 package com.hwzy.app.ui.screens.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun NewsContent() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    var isRefreshing by rememberSaveable { mutableStateOf(false) }
+
+    LaunchedEffect(isRefreshing) {
+        if (isRefreshing) {
+            try {
+                kotlinx.coroutines.delay(1500)
+            } finally {
+                isRefreshing = false
+            }
+        }
+    }
+
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = {
+            isRefreshing = true
+        }
     ) {
-        Text("资讯内容")
+        LazyColumn(Modifier.fillMaxSize()) {
+            item {
+                Text(text = "First item")
+            }
+            items(5) { index ->
+                Text(text = "Item: $index")
+            }
+            item {
+                Text(text = "Last item")
+            }
+        }
     }
 }
